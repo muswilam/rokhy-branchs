5e8cd58c1e7e181bf59b9f3fe86b99708be8a82f
Sully <jackswilam@gmail.com>
2022-06-17 02:17:48 +0200

    Implement notificaton

---
+++ Notify.cs
@@ -0,0 +1,15 @@
+namespace rokhy_branches
+{
+    public class Notify
+    {
+        public void Send()
+        {
+            System.Console.WriteLine("Message sent.");
+        }
+
+        public void Get()
+        {
+            System.Console.WriteLine("Message.");
+        }
+    }
+}
